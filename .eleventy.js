8b0697d23e9e44bee39712e3b06454861a363438
scriptor-pro <bvh@scriptor.pro>
2026-02-20 12:21:35 +0100

    feat: migrate Eleventy project to repository root

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,17 @@
-
 const { JSDOM } = require("jsdom");
 
-module.exports = function(eleventyConfig) {
-
-  eleventyConfig.addCollection("cqjaOrdered", function(collectionApi) {
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addCollection("cqjaOrdered", function (collectionApi) {
     return collectionApi
       .getFilteredByTag("note")
       .sort((a, b) => a.date - b.date);
   });
 
-  eleventyConfig.addCollection("tagStats", function(collectionApi) {
-    let tagCount = {};
+  eleventyConfig.addCollection("tagStats", function (collectionApi) {
+    const tagCount = {};
 
-    collectionApi.getFilteredByTag("note").forEach(item => {
-      (item.data.tags || []).forEach(tag => {
+    collectionApi.getFilteredByTag("note").forEach((item) => {
+      (item.data.tags || []).forEach((tag) => {
         if (tag !== "note") {
           tagCount[tag] = (tagCount[tag] || 0) + 1;
         }
@@ -29,12 +27,12 @@ module.exports = function(eleventyConfig) {
     });
   });
 
-  eleventyConfig.addTransform("externalLinks", function(content) {
+  eleventyConfig.addTransform("externalLinks", function (content) {
     if (this.page.outputPath && this.page.outputPath.endsWith(".html")) {
       const dom = new JSDOM(content);
       const document = dom.window.document;
 
-      document.querySelectorAll("a[href]").forEach(link => {
+      document.querySelectorAll("a[href]").forEach((link) => {
         const href = link.getAttribute("href");
         if (href.startsWith("http")) {
           link.setAttribute("target", "_blank");
@@ -50,7 +48,7 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: ".",
-      output: "_site"
-    }
+      output: "_site",
+    },
   };
 };
